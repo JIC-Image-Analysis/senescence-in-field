9ad355104980f17d15801c037a09e6b88289298e
Matthew Hartley <mhartley@cantab.net>
2016-11-04 10:14:57 +0000

    Fixed run_analysis_desktop to be able to run analysis on data by identifier

--- run_analysis_desktop.sh
+++ run_analysis_desktop.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-PROJECT=senescence-in-field
-CONTAINER=packed-for-cluster
-DATA_ROOT=~/sketchings/$PROJECT/input
-OUTPUT=~/sketchings/$PROJECT/output
-ARGS=155
-
-docker run -it --rm -v `pwd`/scripts:/scripts -v $DATA_ROOT:/data:ro -v $OUTPUT:/output $CONTAINER $ARGS
+DATA=~/mnt/cluster_home/sketchings/senescence-in-field/raw
+PROJECT=~/mnt/cluster_home/sketchings/senescence-in-field/
+CONTAINER=senescence-in-field
+docker run -t --rm -v $PROJECT:/project:ro -v $DATA:/data:ro -v `pwd`/scripts:/scripts:ro -v `pwd`/output:/output $CONTAINER python /scripts/agent.py $1
--- run_docker_container.sh
+++ run_docker_container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 DATA=~/mnt/cluster_home/sketchings/senescence-in-field/raw
+PROJECT=~/mnt/cluster_home/sketchings/senescence-in-field/
 CONTAINER=senescence-in-field
-docker run -it --rm -v $DATA:/data:ro -v `pwd`/scripts:/scripts:ro -v `pwd`/output:/output $CONTAINER
+docker run -it --rm -v $PROJECT:/project:ro -v $DATA:/data:ro -v `pwd`/scripts:/scripts:ro -v `pwd`/output:/output $CONTAINER
