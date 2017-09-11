cc1dd20004d278cced4881b56e99c8be2f20c990
Matthew Hartley <mhartley@cantab.net>
2017-09-11 11:57:10 +0100

    Factor out more dtool utilities

--- scripts/dtoolutils.py
+++ scripts/dtoolutils.py
@@ -1,5 +1,6 @@
 """Helper functions for working with datasets."""
 
+import os
 import shutil
 import tempfile
 
@@ -16,3 +17,31 @@ def temp_working_dir():
         yield working_dir
     finally:
         shutil.rmtree(working_dir)
+
+
+def stage_outputs(
+    outputs,
+    working_dir,
+    dataset,
+    output_dataset,
+    overlays_to_copy,
+    identifier
+):
+
+    for filename, metadata in outputs:
+        src_abspath = os.path.join(working_dir, filename)
+        useful_name = 'out' # dataset.get_overlay('useful_name')[identifier]
+        relpath = os.path.join(useful_name, filename)
+        output_dataset.put_item(src_abspath, relpath)
+
+        # Add 'from' overlay
+        output_dataset.add_item_metadata(relpath, 'from', identifier)
+
+        # Copy overlays
+        for overlay_name in overlays_to_copy:
+            value = dataset.get_overlay(overlay_name)[identifier]
+            output_dataset.add_item_metadata(relpath, overlay_name, value)
+
+        # Add extra metadata
+        for k, v in metadata.items():
+            output_dataset.add_item_metadata(relpath, k, v)
